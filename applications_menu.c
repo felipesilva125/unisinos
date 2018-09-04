f44d31def763be8f01f9b421698ce47299430c9e
Guilherme Menegotto <guilherme@menegotto.com>
2018-09-03 22:08:20 -0300

    Update applications_menu.c

--- applications_menu.c
+++ applications_menu.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+void call_menu(char *bs, char *es, char *ts, char *fs) {
+  printf(" <<< Applications Menu >>> \n 1) Web Browser %s\n 2) Text Editor %s\n 3) Terminal %s\n 4) Finalizar processo %s\n 5)quit \n", bs, es, ts, fs);
+}
 int main() {
  
  unsigned char option = 0;
   
  while(option != 5) {
+    call_menu("","","","");
+    scanf("%c", option);
+  
+    switch(option) {
+     case 1:
+       break;
     
+     case 2:
+      break;
+      
+     case 3:
+      break;
+      
+     case 4:
+      break;
+      
+     case 5:
+      break;
+      
+      defaut:
+      break;
+    }
  } 
 }
